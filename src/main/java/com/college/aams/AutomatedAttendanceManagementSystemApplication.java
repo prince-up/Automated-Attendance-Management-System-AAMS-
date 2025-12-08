1615203a721ac6a915d95edd39f9ebb5019817d9
Prince Yadav <princeupadhyay2121@gmail.com>
2025-12-09 02:53:26 +0530

    AAMS: Initial Spring Boot project code and files added

---
+++ src/main/java/com/college/aams/AutomatedAttendanceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.college.aams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedAttendanceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedAttendanceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/college/aams/AutomatedAttendanceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.aams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedAttendanceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
